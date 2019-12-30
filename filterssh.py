c37eda3ac473cef34ad1ee8bf3be716459737a93
DeLaymann <46672748+DeLaymann@users.noreply.github.com>
2019-12-30 15:28:30 +0300

    Update filterssh.py

--- filterssh.py
+++ filterssh.py
@@ -1,5 +1,7 @@
+#! /bin/bash/python2.7
+#  -*- coding: UTF-8 -*-
 from scapy.all import *
-import os
+import subprocess, os
 
 load_layer('ssh')
 bind_layers(TCP, SSH, dport=22)
@@ -8,23 +10,29 @@ bind_layers(SSH, SSHMessage)
 bind_layers(SSHMessage, SSHKexInit, {'type': 0x14})
 bind_layers(SSHMessage, SSHDisconnect, {'type': 0x01})
 bind_layers(SSH, SSHEncryptedPacket)
-filter ='tcp and src host 178.128.242.4'
+filter ='tcp'
 
 def checkSSH(pkg):
     if pkg.haslayer(SSHIdent):
         port = str(pkg[TCP].dport)
         ip = str(pkg[IP].src)
-        cmd = 'iptables -A OUTPUT -p tcp -d '+ ip + ' --dport ' + port + ' -j DROP'
-        print('!!! SSH DETECTED on PORT: ' + port + ' from IP: ' + ip + ' !!!')
+        #cmd = 'iptables -A FORWARD -p tcp -m '+ ip + ' --dport ' + port + ' -j DROP'
+        #cmd = 'ipset -T drop_ssh ' + ip
+        set = 'ipset -A drop_ssh ' + ip
         #res = os.system(cmd)
-        return cmd
-    else: print(str(pkg[IP].proto) + ': ' + str(pkg[IP].src) + ':' + str(pkg[TCP].sport) + ' => ' + pkg[IP].dst + ':'+ str(pkg[TCP].dport))
+        print('--- SSH detected on port: ' + port + ' from IP: ' + ip + '! ---')
+        res = subprocess.Popen(['ipset -T drop_ssh ' + ip ], stdout=subprocess.PIPE, shell=True)
+        output = res.stdout.read()
+        (out, err) = res.communicate()
+        p_status = res.wait()
+        print (output)
+        print (out)
+        print (err)
+        if ip + ' is NOT in set drop_ssh.' in out:
+            os.system(set)
+            print('--- IP ' + ip + ' is BLOCKED! ---')
+        else: print('--- IP ' + ip + ' already in list drop_ssh! ---')
+    else:
+        print(filter + ': ' + str(pkg[IP].src) + ':' + str(pkg[TCP].sport) + ' => ' + pkg[IP].dst+':'+ str(pkg[TCP].dport))
 
 sniff(iface='eth0', filter=filter, prn=checkSSH)
-
-
-
-
-
-
-
